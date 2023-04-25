18f5aa3a6ce89e998df3e66bf940c67d97614032
Camilo López <camiloj.lopez79@gmail.com>
2023-04-25 10:33:17 -0400

    Trabajar con nuestro formulario y estado

--- src/components/ui/NewEntry.jsx
+++ src/components/ui/NewEntry.jsx
@@ -1,8 +1,24 @@
+import { useState } from 'react';
 import { Box, Button, TextField } from '@mui/material';
 import SaveOutlinedIcon from '@mui/icons-material/SaveOutlined';
 import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
 
 export const NewEntry = () => {
+
+  const [ isAdding, setIsAdding ] = useState( false );
+  const [ inputValue, setInputValue ] = useState( '' );
+  const [ touched, setTouched ] = useState( false );
+
+  const onTextFieldChanges = ( event ) => {
+    setInputValue( event.target.value );
+  }
+
+  const onSave = () => {
+    if ( inputValue.length === 0 ) return;
+
+    console.log({ inputValue });
+  }
+
   return (
     <Box
       sx={{
@@ -10,43 +26,61 @@ export const NewEntry = () => {
         paddingX: 1
       }}
     >
-      <Button
-        fullWidth
-        startIcon={ <AddCircleOutlineOutlinedIcon /> }
-        variant='outlined'
-      >
-        Agregar tarea
-      </Button>
+      {
+        isAdding
+          ? (
+            <>
+              <TextField 
+                fullWidth
+                sx={{
+                  marginTop: 2,
+                  marginBottom: 1
+                }}
+                placeholder='Nueva entrada'
+                label='Nueva entrada'
+                multiline
+                autoFocus
+                helperText={ inputValue.length <= 0 && touched && 'Ingrese un valor' }
+                error={ inputValue.length <= 0 && touched }
+                value={ inputValue }
+                onChange={ onTextFieldChanges }
+                onBlur={ () => setTouched( true ) }
+              />
 
-      <TextField 
-        fullWidth
-        sx={{
-          marginTop: 2,
-          marginBottom: 1
-        }}
-        placeholder='Nueva entrada'
-        label='Nueva entrada'
-        multiline
-        helperText='Ingrese un valor'
-      />
-
-      <Box
-        display='flex'
-        justifyContent='space-between'
-      >
-        <Button
-          variant='text'
-        >
-            Cancelar
-        </Button>
-        <Button
-          variant='outlined'
-          color='secondary'
-          endIcon={ <SaveOutlinedIcon /> }
-        >
-          Guardar
-        </Button>
-      </Box>
+              <Box
+                display='flex'
+                justifyContent='space-between'
+              >
+                <Button
+                  variant='text'
+                  onClick={ () => setIsAdding( false ) }
+                >
+                  Cancelar
+                </Button>
+                <Button
+                  variant='outlined'
+                  color='secondary'
+                  endIcon={ <SaveOutlinedIcon /> }
+                  onClick={ onSave }
+                >
+                  Guardar
+                </Button>
+              </Box>
+            </>
+          )
+          : (
+            <>
+              <Button
+                fullWidth
+                startIcon={ <AddCircleOutlineOutlinedIcon /> }
+                variant='outlined'
+                onClick={ () => setIsAdding( true ) }
+              >
+                Agregar tarea
+              </Button>
+            </>
+          )
+      }
     </Box>
   );
 }
