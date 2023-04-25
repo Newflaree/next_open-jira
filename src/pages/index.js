38883d003c84e663b32cafdb3c0ef1b7d9a1eb2b
Camilo López <camiloj.lopez79@gmail.com>
2023-04-25 02:07:35 -0400

    Diseño de la pantalla pricipal - index.jsx

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import { Inter } from 'next/font/google';
-import { Typography } from '@mui/material';
+// Material UI
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  Grid
+} from '@mui/material';
 // Layouts
 import { MainLayout } from '@/components/layouts';
 
@@ -7,8 +13,62 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function HomePage() {
   return (
-    <MainLayout>
-      <Typography variant='h1' color='primary'>Hello World</Typography>
+    <MainLayout title='Home - OpenJira'>
+      <Grid
+        container
+        spacing={ 2 }
+      >
+        { /* GridItem */ }
+        <Grid
+          item
+          xs={ 12 }
+          sm={ 4 }
+        >
+          <Card
+            sx={{
+              height: 'calc( 100vh - 100px )'
+            }}
+          >
+            <CardHeader title='Pendientes' />
+
+            <CardContent>
+              { /* Agregar una nueva entrada */ }
+              { /* Listado de las entradas */ }
+            </CardContent>
+          </Card>
+        </Grid>
+        { /* GridItem */ }
+        { /* GridItem */ }
+        <Grid
+          item
+          xs={ 12 }
+          sm={ 4 }
+        >
+          <Card
+            sx={{
+              height: 'calc( 100vh - 100px )'
+            }}
+          >
+            <CardHeader title='En Progreso' />
+          </Card>
+        </Grid>
+        { /* GridItem */ }
+        { /* GridItem */ }
+        <Grid
+          item
+          xs={ 12 }
+          sm={ 4 }
+        >
+          <Card
+            sx={{
+              height: 'calc( 100vh - 100px )'
+            }}
+          >
+            <CardHeader title='Terminadas' />
+          </Card>
+        </Grid>
+        { /* GridItem */ }
+      </Grid>
     </MainLayout>
   );
 }
